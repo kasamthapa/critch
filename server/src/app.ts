2833019210d46958ea9f0f2cf8dbbb035e22629f
Kasam Thapa Magar <kasamthapa89@gmail.com>
2026-03-14 13:14:38 +0545

    chore: add CORS configuration to express app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import cors from "cors";
 import userAuth from "./routes/user.route";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  }),
+);
 app.use(express.json());
 app.use("/api/v1/auth", userAuth);
 app.get("/", (req, res) => {
