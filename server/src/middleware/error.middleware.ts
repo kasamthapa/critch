d965064f46dc8bd072bbec244f7794f2cdf99b73
Kasam Thapa Magar <kasamthapa89@gmail.com>
2026-03-31 16:54:48 +0545

    updated error middleware to handle json verification errors

--- server/src/middleware/error.middleware.ts
+++ server/src/middleware/error.middleware.ts
@@ -8,14 +8,22 @@ export const errorMiddleware = (
   res: Response,
   next: NextFunction,
 ) => {
+  console.error(err);
   if (err instanceof ZodError) {
     return res
       .status(400)
       .json({ message: "Validation failed", errors: err.issues });
   }
+  if (err.name === "TokenExpiredError") {
+    return res.status(401).json({ message: "Token expired" });
+  }
+  if (err.name === "JsonWebTokenError") {
+    return res.status(401).json({ message: "Invalid Token" });
+  }
   if (err instanceof ApiError) {
     return res.status(err.statusCode).json({ message: err.message });
   }
+
   return res.status(500).json({
     message: "Internal server error",
   });
