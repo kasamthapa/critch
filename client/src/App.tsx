3470b63c019a5ca1f7d465629e6d893d9bd6748f
Kasam Thapa Magar <kasamthapa89@gmail.com>
2026-04-24 06:43:40 +0545

    update useContext to useAuth in ProtectedRoute

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,8 +10,8 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route element={<ProtectedRoutes />}>
-            <Route path="/" element={<Home />} />
             <Route path="/dashboard" />
             <Route path="/projects/new" />
           </Route>
