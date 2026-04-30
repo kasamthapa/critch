5fc59e5873014d4906beb3fceeb2b882be01867c
Kasam Thapa Magar <kasamthapa89@gmail.com>
2026-04-30 21:27:31 +0545

    feat:getOneProject api function created

--- client/src/api/project.api.ts
+++ client/src/api/project.api.ts
@@ -1,9 +1,18 @@
-import type { GetAllProjectResponse } from "../types/project.types";
+import type {
+  GetAllProjectResponse,
+  GetOneProjectResponse,
+} from "../types/project.types";
 import { api } from "./axiosInstance";
 
 export const getProjects = async (
   tag?: string,
 ): Promise<GetAllProjectResponse> => {
   const response = await api.get("/projects", { params: tag ? { tag } : {} });
+  return response.data;
+};
+export const getProject = async (
+  id: string,
+): Promise<GetOneProjectResponse> => {
+  const response = await api.get(`/projects/${id}`);
   return response.data;
 };
