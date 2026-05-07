56ef61e7f422bb048a4466302d7aa03e3bbe9333
Kasam Thapa Magar <kasamthapa89@gmail.com>
2026-05-07 10:24:19 +0545

    feat:added createReview function api

--- client/src/api/project.api.ts
+++ client/src/api/project.api.ts
@@ -2,6 +2,10 @@ import type {
   GetAllProjectResponse,
   GetOneProjectResponse,
 } from "../types/project.types";
+import type {
+  CreateReviewRequest,
+  CreateReviewResponse,
+} from "../types/review.types";
 import { api } from "./axiosInstance";
 
 export const getProjects = async (
@@ -14,5 +18,22 @@ export const getOneProject = async (
   id: string,
 ): Promise<GetOneProjectResponse> => {
   const response = await api.get(`/projects/${id}`);
+  return response.data;
+};
+export const createReview = async ({
+  projectId,
+  codeQuality,
+  uiDesign,
+  ideaScore,
+  documentation,
+  comment,
+}: CreateReviewRequest): Promise<CreateReviewResponse> => {
+  const response = await api.post(`/projects/${projectId}/reviews`, {
+    codeQuality,
+    uiDesign,
+    ideaScore,
+    documentation,
+    comment,
+  });
   return response.data;
 };
