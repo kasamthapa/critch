fe2ee2ee23939800342c325679050d9299b37b6c
Kasam Thapa Magar <kasamthapa89@gmail.com>
2026-05-17 00:27:34 +0545

    configure tailwind and style userProfilePage

--- client/src/pages/UserProfile.tsx
+++ client/src/pages/UserProfile.tsx
@@ -35,12 +35,15 @@ function UserProfile() {
   }
 
   return (
-    <div>
+    <div className=" min-h-screen flex  justify-center items-center">
       {isLoading
         ? "Loading...."
         : userProfile && (
-            <div>
-              <div>
+            <div className="m-5  w-full max-w-2xl ">
+              <h1 className="text-3xl text-center mb-3 font-bold">
+                UserProfile
+              </h1>
+              <div className="flex w-full border-2 border-black rounded mb-3">
                 <div className="userInfo">
                   <img src={userProfile.avatarUrl} alt="userProfile" />
                   <span>upload Avatar</span>
@@ -49,26 +52,38 @@ function UserProfile() {
                   <p>{userProfile.username}</p>
                 </div>
               </div>
-              <div className="bio">
+              <div className="bio w-full border-2 border-black rounded mb-3">
                 <p>{userProfile.bio}</p>
               </div>
-              <div>
+              <div className="reputation w-full border-2 border-black rounded font-extrabold mb-3 text-red-600">
                 <p>ReputationScore:{userProfile.reputationScore}</p>
-                <div className="reviews">
-                  <p>Reviews</p>
-                  <p>Received:{userProfile.reviewCount}</p>
-                  <p>Given:{userProfile?.reviewGivenCount}</p>
-                </div>
               </div>
+              <div className="reviews w-full border-2 border-black rounded mb-3">
+                <p className="font-bold border-b-2 border-black">Reviews</p>
+                <p>Received:{userProfile.reviewCount}</p>
+                <p>Given:{userProfile?.reviewGivenCount}</p>
+              </div>
+
               <div>
-                <p>Projects</p>
+                <p className="font-bold border-b-2 mb-3 ">Projects</p>
                 {userProfile.projects.map((p) => (
-                  <div key={p.id}>
-                    <p>{p.title}</p>
-                    <p>AverageRating:{p.avgRating}</p>
-                    <button onClick={() => handleProjectClick(p.id)}>
-                      View Project
-                    </button>
+                  <div
+                    key={p.id}
+                    className="w-full border-2 border-black rounded flex justify-between mb-2"
+                  >
+                    <div>
+                      {" "}
+                      <p>{p.title}</p>
+                      <p>AverageRating:{p.avgRating}</p>
+                    </div>
+                    <div>
+                      <button
+                        onClick={() => handleProjectClick(p.id)}
+                        className="border-black border-2 cursor m-1 rounded p-1"
+                      >
+                        View Project
+                      </button>
+                    </div>
                   </div>
                 ))}
               </div>
