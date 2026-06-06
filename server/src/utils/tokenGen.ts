14a4d5bb43a67e1e4ca6914a32248d01699436a5
Kasam Thapa Magar <kasamthapa89@gmail.com>
2026-06-06 23:09:26 +0545

    fix:changed token expirty

--- server/src/utils/tokenGen.ts
+++ server/src/utils/tokenGen.ts
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/env";
 
 export function generateAccessToken(userId: string) {
-  return jwt.sign({ userId }, JWT_SECRET, { expiresIn: "10s" });
+  return jwt.sign({ userId }, JWT_SECRET, { expiresIn: "7d" });
 }
 
 export function generateRefreshToken(userId: string) {
